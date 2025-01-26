96240ebc4dc2f47006fbfadf9c7e50c8dd020486
François Paupier <francois.paupier@gmail.com>
2025-01-26 17:55:31 +0100

    update(test 4WD)

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,57 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
 #include <Arduino.h>
 
+// Rear Motors (L298N 1)
+#define REAR_LEFT_IN1  12
+#define REAR_LEFT_IN2  13
+#define REAR_RIGHT_IN3 14
+#define REAR_RIGHT_IN4 15
 
-// Define motor control pins (adjust based on your wiring)
-#define MOTOR1_IN1 12
-#define MOTOR1_IN2 13
-#define MOTOR2_IN1 14
-#define MOTOR2_IN2 15
-// Repeat for MOTOR3 and MOTOR4 as needed
+// Front Motors (L298N 2)
+#define FRONT_LEFT_IN1 16
+#define FRONT_LEFT_IN2 17
+#define FRONT_RIGHT_IN3 18
+#define FRONT_RIGHT_IN4 19
 
 void setup() {
-    pinMode(MOTOR1_IN1, OUTPUT);
-    pinMode(MOTOR1_IN2, OUTPUT);
-    pinMode(MOTOR2_IN1, OUTPUT);
-    pinMode(MOTOR2_IN2, OUTPUT);
+  // Initialize all motor control pins
+  pinMode(REAR_LEFT_IN1, OUTPUT);
+  pinMode(REAR_LEFT_IN2, OUTPUT);
+  pinMode(REAR_RIGHT_IN3, OUTPUT);
+  pinMode(REAR_RIGHT_IN4, OUTPUT);
+  pinMode(FRONT_LEFT_IN1, OUTPUT);
+  pinMode(FRONT_LEFT_IN2, OUTPUT);
+  pinMode(FRONT_RIGHT_IN3, OUTPUT);
+  pinMode(FRONT_RIGHT_IN4, OUTPUT);
 }
 
-
-void motorForward(int in1, int in2) {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
+// Motor control functions
+void setMotor(int in1, int in2, bool forward) {
+  digitalWrite(in1, forward ? HIGH : LOW);
+  digitalWrite(in2, forward ? LOW : HIGH);
 }
 
-void motorStop(int in1, int in2) {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
+void stopMotor(int in1, int in2) {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+}
+
+// Test all motors
+void testAllMotors() {
+  // All motors forward (2 seconds)
+  setMotor(REAR_LEFT_IN1, REAR_LEFT_IN2, true);
+  setMotor(REAR_RIGHT_IN3, REAR_RIGHT_IN4, true);
+  setMotor(FRONT_LEFT_IN1, FRONT_LEFT_IN2, true);
+  setMotor(FRONT_RIGHT_IN3, FRONT_RIGHT_IN4, true);
+  delay(2000);
+
+  // Stop all motors (1 second)
+  stopMotor(REAR_LEFT_IN1, REAR_LEFT_IN2);
+  stopMotor(REAR_RIGHT_IN3, REAR_RIGHT_IN4);
+  stopMotor(FRONT_LEFT_IN1, FRONT_LEFT_IN2);
+  stopMotor(FRONT_RIGHT_IN3, FRONT_RIGHT_IN4);
+  delay(1000);
 }
 
 void loop() {
-    // Test Motor 1 Forward
-    motorForward(MOTOR1_IN1, MOTOR1_IN2);
-    delay(2000);
-    motorStop(MOTOR1_IN1, MOTOR1_IN2);
-    delay(1000);
-
-    // Test Motor 2 Forward
-    motorForward(MOTOR2_IN1, MOTOR2_IN2);
-    delay(2000);
-    motorStop(MOTOR2_IN1, MOTOR2_IN2);
-    delay(1000);
+  testAllMotors();
 }
